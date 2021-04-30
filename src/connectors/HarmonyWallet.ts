13c651b4a3aebeaffc9c2c6bbc0443b8dcbaedf5
TheerapakG <theerapakg@gmail.com>
2021-04-30 15:32:01 +0700

    remove swoop dependency

--- src/connectors/HarmonyWallet.ts
+++ src/connectors/HarmonyWallet.ts
@@ -1,22 +1,31 @@
-import { Harmony } from '@harmony-js/core'
+import { ExtensionAccount, Harmony, HarmonyExtension } from '@harmony-js/core'
 import { HarmonyAddress } from '@harmony-js/crypto'
 import { AbstractConnectorArguments, ConnectorUpdate } from '@web3-react/types'
 import { AbstractConnector } from '@web3-react/abstract-connector'
-import { Hmy, OneWallet } from '@swoop-exchange/utils'
 
 class HmyWalletProvider {
-  private wallet: OneWallet
-  private realprovider: any
+  private ext: HarmonyExtension
+  public hmy: Harmony
 
-  constructor(wallet: OneWallet) {
-    this.wallet = wallet
-    this.realprovider = this.wallet.client.client.messenger.provider
+  constructor(ext: HarmonyExtension) {
+    this.ext = ext
+    this.hmy = new Harmony(ext.provider.url, {
+      chainType: ext.messenger.chainType,
+      chainId: 1
+    })
+
+    this.ext.provider = this.hmy.messenger.provider
+    this.ext.messenger = this.hmy.messenger
+    this.ext.setShardID(0)
+    this.ext.wallet.messenger = this.hmy.messenger
+    this.ext.blockchain.messenger = this.hmy.messenger
+    this.ext.transactions.messenger = this.hmy.messenger
+    this.ext.contracts.wallet = this.hmy.wallet
   }
 
   public async send(payload: any, callback?: any): Promise<any> {
     if ('method' in payload && payload.method === 'eth_sendTransaction') {
-      const hmy = this.wallet.client.client as Harmony
-      const txn = hmy.transactions.newTx({
+      const txn = this.ext.transactions.newTx({
         from: new HarmonyAddress(payload.params[0].from).bech32,
         to: new HarmonyAddress(payload.params[0].to).bech32,
         data: payload.params[0].data,
@@ -26,59 +35,77 @@ class HmyWalletProvider {
         toShardID: 0
       })
 
-      const signedTxn = await this.wallet.signTransaction(txn)
-      return await this.realprovider.send(
+      const signedTxn = await (window as any).onewallet.signTransaction(txn)
+      return await this.ext.provider.send(
         {
           method: 'hmy_sendRawTransaction',
-          params: [signedTxn.rawTransaction],
+          params: [signedTxn['rawTransaction']],
           id: payload.id,
           jsonrpc: payload.jsonrpc
         },
         callback
       )
     }
-    return await this.realprovider.send(payload, callback)
+    return await this.ext.provider.send(payload, callback)
   }
 }
 
 export class HmyWalletConnector extends AbstractConnector {
-  private onewallet: OneWallet
+  private ext?: HarmonyExtension
+  private provider?: HmyWalletProvider
+  private account: string | null
+  private auth: boolean
 
   constructor(kwargs: AbstractConnectorArguments) {
     super(kwargs)
-    // TODO: get network str from id
-    const network = 'mainnet'
-    this.onewallet = new OneWallet(network, new Hmy(network))
+    this.account = null
+    this.auth = false
   }
 
   public async activate(): Promise<ConnectorUpdate> {
-    await this.onewallet.signIn()
-    return {
-      provider: new HmyWalletProvider(this.onewallet),
-      chainId: parseInt((await this.onewallet.client.client.messenger.send('eth_chainId', [], 'eth')).result),
-      account: this.onewallet.base16Address
-    }
-  }
-
-  public async getProvider(): Promise<any> {
-    return new HmyWalletProvider(this.onewallet)
-  }
-
-  public async getChainId(): Promise<number | string> {
-    return parseInt((await this.onewallet.client.client.messenger.send('eth_chainId', [], 'eth')).result)
-  }
-
-  public async getAccount(): Promise<null | string> {
-    return this.onewallet.base16Address
-  }
-
-  public deactivate() {
-    this.onewallet.signOut().catch((error: Error) => {
-      console.log(error)
+    this.ext = new HarmonyExtension((window as any).onewallet)
+    return this.ext.login().then(async (account: ExtensionAccount) => {
+      if (this.ext) {
+        this.account = this.ext.crypto.fromBech32(account.address)
+        this.auth = true
+        this.provider = new HmyWalletProvider(this.ext)
+        return {
+          provider: this.provider,
+          chainId: parseInt((await this.provider.hmy.messenger.send('eth_chainId', [], 'eth', 0)).result),
+          account: this.account
+        }
+      }
+      return {}
     })
   }
 
+  public async getProvider(): Promise<any> {
+    return this.provider
+  }
+
+  public async getChainId(): Promise<number | string> {
+    return this.provider ? parseInt((await this.provider.send('eth_chainId')).result) : -1
+  }
+
+  public async getAccount(): Promise<null | string> {
+    return this.account
+  }
+
+  public deactivate() {
+    if (this.ext) {
+      this.ext
+        .logout()
+        .then(() => {
+          this.auth = false
+          return Promise.resolve()
+        })
+        .catch((error: Error) => {
+          console.log(error)
+        })
+    }
+  }
+
   public async isAuthorized(): Promise<boolean> {
-    return this.onewallet.isAuthorized
+    return this.auth
   }
 }
